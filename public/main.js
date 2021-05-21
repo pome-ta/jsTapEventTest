00c5eb9d63993343217f56df3001725d9f755d9e
pome-ta <pome-ta@users.noreply.github.com>
2021-05-22 04:20:55 +0900

    hummm....

--- public/main.js
+++ public/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hofe');
+const {tapDown, tapMove, tapUp} = {
+  tapDown: typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
+  tapMove: typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
+  tapUp: typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup',
+}
+
+const btns = document.getElementsByClassName('btn');
+//console.log(btns);
+
+
+document.body.addEventListener(tapDown, function(e) {
+  console.log(e.target.tagName);
+});
+
+/*
+document.addEventListener(tapDown, (e) => {
+  console.log(this);
+});
+*/
+
+
